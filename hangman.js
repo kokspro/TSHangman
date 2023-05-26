c1e73968b77407cba0cd5a62be0b6f6b295acba5
kokspro <koks.programming@gmail.com>
2023-05-26 13:57:54 -0500

    Type and Object created for global variables

--- hangman.js
+++ hangman.js
@@ -1,4 +1,15 @@
 "use strict";
+// let hangmanImage = document.getElementById('hangmanImage') as HTMLImageElement;
+// let secretWord = document.getElementById('secretWord') as HTMLDivElement;
+// let guessBoard = document.getElementById('guessBoard') as HTMLDivElement;
+// let startBtn = document.getElementById('startBtn') as HTMLButtonElement;
+// let resetBtn = document.getElementById('resetBtn') as HTMLButtonElement;
+// let mode = document.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
+// let lives: number = 7; 
+// let word: string[];
+// let letterPlaceholder: NodeList; 
+// let secretWordLength: number; 
+// let lettersCorrect: number = 0;
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,69 +19,70 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let hangmanImage = document.getElementById('hangmanImage');
-let secretWord = document.getElementById('secretWord');
-let guessBoard = document.getElementById('guessBoard');
-let startBtn = document.getElementById('startBtn');
-let resetBtn = document.getElementById('resetBtn');
-let mode = document.querySelectorAll('input');
-let lives = 7;
-let word;
-let letterPlaceholder;
-let secretWordLength;
-let lettersCorrect = 0;
-startBtn.addEventListener('click', disableMode);
-resetBtn.addEventListener('click', () => { location.reload(); });
+const dom = {
+    hangmanImage: document.getElementById('hangmanImage'),
+    secretWord: document.getElementById('secretWord'),
+    guessBoard: document.getElementById('guessBoard'),
+    startBtn: document.getElementById('startBtn'),
+    resetBtn: document.getElementById('resetBtn'),
+    mode: document.querySelectorAll('input')
+};
+const vars = {
+    lives: 7,
+    lettersCorrect: 0
+};
+dom.startBtn.addEventListener('click', disableMode);
+dom.resetBtn.addEventListener('click', () => { location.reload(); });
 function disableMode() {
-    mode.forEach(function (radio) {
+    dom.mode.forEach(function (radio) {
         radio.disabled = true;
     });
-    startBtn.disabled = true;
+    dom.startBtn.disabled = true;
     getWord();
 }
 function getWord() {
     return __awaiter(this, void 0, void 0, function* () {
-        if (mode[0].checked) {
+        if (dom.mode[0].checked) {
             let wordLength = Math.floor(Math.random() * (9 - 6)) + 7;
             yield fetch(`https://random-word-api.vercel.app/api?words=1&length=${wordLength}`)
                 .then((response) => response.json())
-                .then((response) => (word = response.toString().toUpperCase().split('')));
+                .then((response) => (vars.word = response.toString().toUpperCase().split(''))); //variables
         }
-        else if (mode[1].checked)
+        else if (dom.mode[1].checked)
             yield fetch('https://random-word-api.herokuapp.com/word')
                 .then((response) => response.json())
-                .then((response) => (word = response.toString().toUpperCase().split('')));
-        secretWordLength = word.length;
+                .then((response) => (vars.word = response.toString().toUpperCase().split(''))); //variables
+        vars.secretWordLength = vars.word.length; //variables 2 ? 1
         populateWord();
         populateBoard();
     });
 }
 function populateWord() {
-    word.forEach(function (e) {
+    vars.word.forEach(function (e) {
         let letter = document.createElement('span');
         letter.innerHTML = '_';
         fontSize(letter);
-        secretWord.append(letter);
+        dom.secretWord.append(letter);
     });
-    letterPlaceholder = document.querySelectorAll('span');
+    vars.letterPlaceHolder = document.querySelectorAll('span'); //variables
 }
 function fontSize(letter) {
-    if (word.length <= 7)
-        letter.style.fontSize = `calc((80vw / ${word.length}) / 1.9)`;
+    if (vars.word.length <= 7) //variables ?
+        letter.style.fontSize = `calc((80vw / ${vars.word.length}) / 1.9)`; //variables ?
     else
-        letter.style.fontSize = `calc((80vw / ${word.length}) / 1.5)`;
+        letter.style.fontSize = `calc((80vw / ${vars.word.length}) / 1.5)`; // variables ?
 }
 function populateBoard() {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     alphabet.forEach(function (e) {
         let button = document.createElement('button');
         button.innerHTML = e.toUpperCase();
-        guessBoard.append(button);
+        dom.guessBoard.append(button);
         button.addEventListener('click', checkLetter);
     });
 }
 function checkLetter(e) {
-    let currentLettersCorrect = lettersCorrect;
+    let currentLettersCorrect = vars.lettersCorrect; //variables
     let letterToCheck = e.target.innerHTML;
     e.target.disabled = true;
     e.target.classList.add('selected');
@@ -78,29 +90,29 @@ function checkLetter(e) {
     adjustLives(currentLettersCorrect);
 }
 function testLetter(letterToCheck) {
-    for (let i = 0; i < word.length; i++)
-        if (letterToCheck === word[i]) {
-            letterPlaceholder[i].textContent = letterToCheck;
-            lettersCorrect++;
-            if (lettersCorrect === secretWordLength)
-                guessBoard.style.display = 'none';
+    for (let i = 0; i < vars.word.length; i++) //variables !
+        if (letterToCheck === vars.word[i]) {
+            vars.letterPlaceHolder[i].textContent = letterToCheck;
+            vars.lettersCorrect++;
+            if (vars.lettersCorrect === vars.secretWordLength)
+                dom.guessBoard.style.display = 'none';
         }
 }
 function adjustLives(currentLettersCorrect) {
-    if (currentLettersCorrect === lettersCorrect) {
-        lives--;
-        hangmanImage.src = `./Images/Hangman${lives}.jpg`;
-        if (lives === 0) {
-            guessBoard.style.display = 'none';
+    if (currentLettersCorrect === vars.lettersCorrect) {
+        vars.lives--;
+        dom.hangmanImage.src = `./Images/Hangman${vars.lives}.jpg`;
+        if (vars.lives === 0) {
+            dom.guessBoard.style.display = 'none';
             setTimeout(youLose, 500);
         }
     }
 }
 function youLose() {
-    for (let i = 0; i < word.length; i++)
-        if (letterPlaceholder[i].textContent === '_') {
-            letterPlaceholder[i].textContent = word[i];
-            let missedLetter = letterPlaceholder[i];
+    for (let i = 0; i < vars.word.length; i++)
+        if (vars.letterPlaceHolder[i].textContent === '_') {
+            vars.letterPlaceHolder[i].textContent = vars.word[i];
+            let missedLetter = vars.letterPlaceHolder[i];
             missedLetter.style.color = '#1aa7fc';
         }
 }
--- hangman.ts
+++ hangman.ts
@@ -1,58 +1,79 @@
-let hangmanImage = document.getElementById('hangmanImage') as HTMLImageElement;
-let secretWord = document.getElementById('secretWord') as HTMLDivElement;
-let guessBoard = document.getElementById('guessBoard') as HTMLDivElement;
-let startBtn = document.getElementById('startBtn') as HTMLButtonElement;
-let resetBtn = document.getElementById('resetBtn') as HTMLButtonElement;
-let mode = document.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
+// let hangmanImage = document.getElementById('hangmanImage') as HTMLImageElement;
+// let secretWord = document.getElementById('secretWord') as HTMLDivElement;
+// let guessBoard = document.getElementById('guessBoard') as HTMLDivElement;
+// let startBtn = document.getElementById('startBtn') as HTMLButtonElement;
+// let resetBtn = document.getElementById('resetBtn') as HTMLButtonElement;
+// let mode = document.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
+// let lives: number = 7; 
+// let word: string[];
+// let letterPlaceholder: NodeList; 
+// let secretWordLength: number; 
+// let lettersCorrect: number = 0;
 
-let lives: number = 7; 
-let word: string[];
-let letterPlaceholder: NodeList; 
-let secretWordLength: number; 
-let lettersCorrect: number = 0;
+const dom = {
+    hangmanImage: document.getElementById('hangmanImage') as HTMLImageElement,
+    secretWord : document.getElementById('secretWord') as HTMLDivElement,
+    guessBoard : document.getElementById('guessBoard') as HTMLDivElement,
+    startBtn : document.getElementById('startBtn') as HTMLButtonElement,
+    resetBtn : document.getElementById('resetBtn') as HTMLButtonElement,
+    mode : document.querySelectorAll('input') as NodeListOf<HTMLInputElement>
+}
 
-startBtn.addEventListener('click', disableMode);
-resetBtn.addEventListener('click', () => { location.reload() });
+type GameVariables = {
+    lives: number,
+    word?: string[],
+    letterPlaceHolder?: NodeList,
+    secretWordLength?: number,
+    lettersCorrect: number
+}
+
+const vars: GameVariables = {
+    lives : 7,
+    lettersCorrect : 0
+}
+
+dom.startBtn.addEventListener('click', disableMode);
+dom.resetBtn.addEventListener('click', () => { location.reload() });
 
 function disableMode(): void {
-    mode.forEach( function(radio: HTMLInputElement) {
+    dom.mode.forEach( function(radio: HTMLInputElement) {
         radio.disabled = true;
     });
-    startBtn.disabled = true;
+    dom.startBtn.disabled = true;
     getWord();
 }
 
 async function getWord(): Promise<void> {
-    if (mode[0].checked) {
+    if (dom.mode[0].checked) {
         let wordLength: number = Math.floor(Math.random() * (9 - 6)) + 7;
         await fetch(`https://random-word-api.vercel.app/api?words=1&length=${wordLength}`)
         .then((response) => response.json())
-        .then((response) => ( word = response.toString().toUpperCase().split('') ));
-    } else if (mode[1].checked) 
+        .then((response) => ( vars.word = response.toString().toUpperCase().split('') )); //variables
+    } else if (dom.mode[1].checked) 
         await fetch('https://random-word-api.herokuapp.com/word')
         .then((response) => response.json())
-        .then((response) => ( word = response.toString().toUpperCase().split('') ));
+        .then((response) => ( vars.word = response.toString().toUpperCase().split('') )); //variables
 
-    secretWordLength = word.length;
+    vars.secretWordLength = vars.word!.length;  //variables 2 ? 1
     populateWord();
     populateBoard();
 }
 
 function populateWord(): void { 
-    word.forEach( function (e: string) {
+    vars.word!.forEach( function (e: string) {  //variables ?
         let letter: HTMLSpanElement = document.createElement('span');
         letter.innerHTML = '_';
         fontSize(letter);
-        secretWord.append(letter);
+        dom.secretWord.append(letter);
     });
-    letterPlaceholder = document.querySelectorAll('span') as NodeListOf<HTMLSpanElement>;
+    vars.letterPlaceHolder = document.querySelectorAll('span') as NodeListOf<HTMLSpanElement>;  //variables
 }
 
 function fontSize(letter: HTMLSpanElement): void {
-    if (word.length <= 7)
-        letter.style.fontSize = `calc((80vw / ${word.length}) / 1.9)`;
+    if (vars.word!.length <= 7)  //variables ?
+        letter.style.fontSize = `calc((80vw / ${vars.word!.length}) / 1.9)`;  //variables ?
     else 
-        letter.style.fontSize = `calc((80vw / ${word.length}) / 1.5)`;
+        letter.style.fontSize = `calc((80vw / ${vars.word!.length}) / 1.5)`; // variables ?
 }
 
 function populateBoard(): void {
@@ -60,13 +81,13 @@ function populateBoard(): void {
     alphabet.forEach( function (e: string) {
         let button = document.createElement('button');
         button.innerHTML = e.toUpperCase();
-        guessBoard.append(button);
+        dom.guessBoard.append(button);
         button.addEventListener('click', checkLetter);
     });
 }
 
 function checkLetter(e: any): void {
-    let currentLettersCorrect: number = lettersCorrect;
+    let currentLettersCorrect: number = vars.lettersCorrect;  //variables
     let letterToCheck: string = e.target.innerHTML;
     e.target.disabled = true;
     e.target.classList.add('selected');
@@ -75,31 +96,31 @@ function checkLetter(e: any): void {
 }
 
 function testLetter(letterToCheck: string): void {
-    for (let i = 0; i < word.length; i++)
-        if (letterToCheck === word[i]) {
-            letterPlaceholder[i].textContent = letterToCheck; 
-            lettersCorrect++;
-            if (lettersCorrect === secretWordLength)
-                guessBoard.style.display = 'none';
+    for (let i = 0; i < vars.word!.length; i++)  //variables !
+        if (letterToCheck === vars.word![i]) {
+            vars.letterPlaceHolder![i].textContent = letterToCheck; 
+            vars.lettersCorrect++;
+            if (vars.lettersCorrect === vars.secretWordLength)
+                dom.guessBoard.style.display = 'none';
         } 
 }
 
 function adjustLives(currentLettersCorrect: number): void {
-    if (currentLettersCorrect === lettersCorrect) {
-        lives--;
-        hangmanImage.src = `./Images/Hangman${lives}.jpg`;
-        if (lives === 0) {
-            guessBoard.style.display = 'none';
+    if (currentLettersCorrect === vars.lettersCorrect) {
+        vars.lives--;
+        dom.hangmanImage.src = `./Images/Hangman${vars.lives}.jpg`;
+        if (vars.lives === 0) {
+            dom.guessBoard.style.display = 'none';
             setTimeout(youLose, 500);
         }
     }
 }
 
 function youLose(): void {
-    for (let i = 0; i < word.length; i++)
-        if (letterPlaceholder[i].textContent === '_') {
-            letterPlaceholder[i].textContent = word[i];
-            let missedLetter = letterPlaceholder[i] as HTMLSpanElement;
+    for (let i = 0; i < vars.word!.length; i++)
+        if (vars.letterPlaceHolder![i].textContent === '_') {
+            vars.letterPlaceHolder![i].textContent = vars.word![i];
+            let missedLetter = vars.letterPlaceHolder![i] as HTMLSpanElement;
             missedLetter.style.color = '#1aa7fc';
         }
 }
